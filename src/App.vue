375933e5ffa17429f48c93bc50f4c4c85a5ea712
HuYang <hy.ccccr@gmail.com>
2025-05-21 13:36:39 +0800

    feat: :tada: init project add start page

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from "vue-router";
+import { useColorMode } from "@vueuse/core";
+
+var mode = useColorMode({
+  disableTransition: false,
+});
+mode.value = "dark";
+</script>
+
+<template>
+  <RouterView />
+</template>
+
+<style>
+body {
+  background-color: transparent !important;
+}
+</style>
---
+++ src/components/CreateTask.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import { open } from "@tauri-apps/plugin-dialog";
+import Button from "@/components/ui/button/Button.vue";
+import {
+  Card,
+  CardTitle,
+  CardHeader,
+  CardContent,
+  CardDescription,
+} from "@/components/ui/card";
+
+async function picker() {
+  const file = await open({
+    multiple: false,
+    directory: true,
+  });
+
+  if (file) {
+    console.log("Selected directory:", file);
+  }
+}
+</script>
+
+<template>
+  <div class="flex items-center justify-center min-h-screen">
+    <Card class="min-w-xs w-[40%] bg-card/60">
+      <CardHeader>
+        <CardTitle>Create event</CardTitle>
+        <CardDescription
+          class="text-base/relaxed text-muted-foreground tracking-wide"
+          >Start from creating a new event: For example, if you are preparing
+          documents for visa application this time, then create a folder named
+          <span class="font-medium text-foreground">"visa"</span
+          >.</CardDescription
+        >
+      </CardHeader>
+      <CardContent class="flex justify-center">
+        <Button @click="picker" class="w-full">Start</Button>
+      </CardContent>
+    </Card>
+  </div>
+</template>
